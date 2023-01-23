77111523fbc81ffb82fb4987347f71ed52eb8bfd
Ola Lekan <lekan.sofuyi01@gmail.com>
2023-01-23 14:54:54 +0100

    Registration Service Mock Testing Completed

--- src/main/java/africa/semicolon/unicoin/registration/token/ConfirmationTokenService.java
+++ src/main/java/africa/semicolon/unicoin/registration/token/ConfirmationTokenService.java
@@ -1,5 +1,7 @@
 package africa.semicolon.unicoin.registration.token;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class ConfirmationTokenService {
     @Autowired
     private ConfirmationTokenRepository confirmationTokenRepository;
